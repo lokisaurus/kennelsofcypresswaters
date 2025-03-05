d50aa9a6e24d6811d0f6ac02c560ce38606986a9
lokisaurus <kialyreaulo@gmail.com>
2025-03-05 15:09:15 -0500

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1DW1i2LofqWPz6OkzfqwCwUpq0xoqj_p5lll40F0Jn30";
 
 
 /* ------------------------------------------------------------------- */
